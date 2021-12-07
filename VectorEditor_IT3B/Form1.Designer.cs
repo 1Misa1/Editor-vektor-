0c045b4577332692408c7a7d0138eb3fb2e582e4
Michal Sekyra <Sekyra.Michal@stud.vassboskovice.cz>
2021-12-07 12:19:45 +0100

    Přidejte soubory projektu.

---
+++ VectorEditor_IT3B/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace VectorEditor_IT3B
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pboxCanvas = new System.Windows.Forms.PictureBox();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pboxCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pboxCanvas
+            // 
+            this.pboxCanvas.BackColor = System.Drawing.Color.White;
+            this.pboxCanvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pboxCanvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pboxCanvas.Location = new System.Drawing.Point(0, 0);
+            this.pboxCanvas.Name = "pboxCanvas";
+            this.pboxCanvas.Size = new System.Drawing.Size(994, 590);
+            this.pboxCanvas.TabIndex = 0;
+            this.pboxCanvas.TabStop = false;
+            this.pboxCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.pboxCanvas_Paint);
+            this.pboxCanvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pboxCanvas_MouseClick);
+            this.pboxCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pboxCanvas_MouseMove);
+            // 
+            // sfd
+            // 
+            this.sfd.FileOk += new System.ComponentModel.CancelEventHandler(this.sfd_FileOk);
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            this.ofd.FileOk += new System.ComponentModel.CancelEventHandler(this.ofd_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 590);
+            this.Controls.Add(this.pboxCanvas);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pboxCanvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pboxCanvas;
+        private System.Windows.Forms.SaveFileDialog sfd;
+        private System.Windows.Forms.OpenFileDialog ofd;
+    }
+}
+
---
+++ VectorEditor_IT3B/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace VectorEditor_IT3B
+{
+    public partial class Form1 : Form
+    {
+        List<PointF> points;
+        Point mousePoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+            points = new List<PointF>();
+        }
+
+        private void pboxCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = e.Location.ToString();
+            mousePoint = e.Location;
+            //pboxCanvas.Refresh();
+        }
+
+        private void pboxCanvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            points.Add(e.Location);
+            pboxCanvas.Refresh();
+        }
+
+        private void pboxCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            if (points.Count > 2)
+            {
+                e.Graphics.DrawLines(Pens.Black, points.ToArray());
+            }
+            foreach (var point in points)
+            {
+                DrawPoint(e.Graphics, point);
+            }
+        }
+
+        private void DrawPoint(Graphics g, PointF point)
+        {
+            int size = 10;
+            g.FillEllipse(Brushes.Blue, point.X - size / 2, point.Y - size / 2, size, size);
+        }
+
+        private void sfd_FileOk(object sender, CancelEventArgs e)
+        {
+            var json = JsonConvert.SerializeObject(points/*, Formatting.Indented*/);
+            File.WriteAllText(sfd.FileName, json);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.S && e.Control)
+            {
+                sfd.ShowDialog();
+            }
+            else if (e.KeyCode == Keys.O && e.Control)
+            {
+                ofd.ShowDialog();
+            }
+        }
+
+        private void ofd_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                var json = File.ReadAllText(ofd.FileName);
+                points = JsonConvert.DeserializeObject<List<PointF>>(json);
+                pboxCanvas.Refresh();
+            }
+            catch
+            {
+                MessageBox.Show("Nepodařilo se otevřít.");
+            }
+        }
+    }
+}
