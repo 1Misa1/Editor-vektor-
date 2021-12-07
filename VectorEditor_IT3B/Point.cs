7467885e5969fb04fe2f4a1ba4b3cd5c19d5b399
Michal Sekyra <Sekyra.Michal@stud.vassboskovice.cz>
2021-12-07 12:53:56 +0100

    Tída Shape, Point, přidání dědičnosti

---
+++ VectorEditor_IT3B/Point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace VectorEditor_IT3B
+{
+    public class Point: Shape /// : něco je dědičnost
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public Color Color { get; set; }
+        public int Diameter { get; set; }
+
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+            Color = Color.Black;
+            Diameter = 8;
+
+        }
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+            Color = Color.Black;
+            Diameter = 8;
+        }
+        public Point(float x, float y, Color color, int diameter)
+        {
+            X = x;
+            Y = y;
+            this.Color = color;
+            Diameter = diameter;
+        }
+
+      
+
+        public override void Draw(Graphics graphics)
+        {
+            
+        }
+
+    }
+}
---
+++ VectorEditor_IT3B/Shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace VectorEditor_IT3B
+{
+    public abstract class Shape
+    {
+        public abstract void Draw(Graphics graphics); // pokuď něco chce dědit třídu musí mít Draw, virutal muže mít nadřazenou třídu, abstract ne 
+    }
+}
